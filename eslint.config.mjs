5b76c9a89be10cfa88382b9ded353c07e9c3a9c3
mhaibaraai <mhaibaraai@gmail.com>
2025-11-20 10:18:36 +0800

    chore: 更新项目配置和依赖

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,8 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   rules: {
+    'style/comma-dangle': 'off',
     'ts/no-empty-object-type': 'off',
+    'vue/max-attributes-per-line': ['error', { singleline: 3, multiline: 1 }],
   },
 })
